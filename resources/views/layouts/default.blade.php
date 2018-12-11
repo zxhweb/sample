996b8232b4d0d82e2f8384af47c227a728508465
zxhweb <zxhweb@qq.com>
2018-12-11 06:17:25 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,14 @@
 <html>
 <head>
     <title>@yield('title', 'zxhweb App') - Laravel 入门教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+    @include('layouts._header')
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+
 </body>
 </html>
